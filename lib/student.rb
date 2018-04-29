a9a3be6b0b35076232a63b8c75906286f7fc80ec
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-29 23:35:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
 
 
   def self.create_from_collection(students_array)
-      Scraper.scrape_index_page
+      Scraper.scrape_index_page(students_array)
   end
 
   def add_student_attributes(attributes_hash)
