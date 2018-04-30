901676a69b8eb68c91d26d956e19ea0f5a98e48e
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-30 00:14:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-  #  Scraper.scrape_profile_page(attributes_hash)
+   Scraper.scrape_profile_page(attributes_hash)
   end
 
   def self.all
