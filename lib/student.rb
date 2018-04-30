61d2f51bfc41642db43afa8e8d66524c064636d2
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-30 00:21:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,8 @@ class Student
   def add_student_attributes(attributes_hash)
    attributes_hash.each do |key, value|
      self.send("#{key}=", value)
-  end
+   end
+ end
 
   def self.all
     @@all
